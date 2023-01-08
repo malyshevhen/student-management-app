327b20618d0948bcc09558333645f08f9158638e
Evhen <102974676+KakogoCherta@users.noreply.github.com>
2023-01-08 12:11:51 +0200

    refactor: DDL script.

--- src/main/resources/scripts/ddl/Table_creation.sql
+++ src/main/resources/scripts/ddl/Table_creation.sql
@@ -2,32 +2,27 @@ DROP TABLE IF EXISTS students_courses;
 DROP TABLE IF EXISTS students;
 DROP TABLE IF EXISTS groups;
 DROP TABLE IF EXISTS courses;
-CREATE TABLE students
-(
-    student_id SERIAL,
-    group_id   INT,
-    first_name VARCHAR(255) NOT NULL,
-    last_name  VARCHAR(255) NOT NULL,
-    PRIMARY KEY (student_id)
-);
 CREATE TABLE groups
 (
-    group_id   SERIAL,
-    group_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (group_id)
+    group_id   SERIAL PRIMARY KEY,
+    group_name VARCHAR(255) NOT NULL UNIQUE
 );
 CREATE TABLE courses
 (
-    course_id          SERIAL,
-    course_name        VARCHAR(255) NOT NULL,
-    course_description VARCHAR(255) NOT NULL,
-    PRIMARY KEY (course_id)
+    course_id          SERIAL PRIMARY KEY,
+    course_name        VARCHAR(255) NOT NULL UNIQUE,
+    course_description VARCHAR(255) NOT NULL
+);
+CREATE TABLE students
+(
+    student_id SERIAL PRIMARY KEY,
+    group_id   INT REFERENCES groups (group_id),
+    first_name VARCHAR(255) NOT NULL,
+    last_name  VARCHAR(255) NOT NULL
 );
 CREATE TABLE students_courses
 (
-    student_id INT NOT NULL,
-    course_id  INT NOT NULL,
-    PRIMARY KEY (student_id, course_id),
-    FOREIGN KEY (student_id) REFERENCES students (student_id),
-    FOREIGN KEY (course_id) REFERENCES courses (course_id)
+    student_id INT REFERENCES students (student_id),
+    course_id  INT REFERENCES courses (course_id),
+    PRIMARY KEY (student_id, course_id)
 );
